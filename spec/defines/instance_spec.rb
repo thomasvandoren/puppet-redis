663b654695dba28cbd513d41120ea1e2a3039f78
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-07-13 00:39:10 -0700

    Update template for redis saves and add spec tests for saves.

--- spec/defines/instance_spec.rb
+++ spec/defines/instance_spec.rb
@@ -79,4 +79,37 @@ describe 'redis::instance', :type => 'define' do
       should contain_file('redis-init-6900').with_content(/^REDIS_BIND_ADDRESS="10.1.2.3"$/)
     end # it
   end # context
+
+  context "On Debian systems with no saves" do
+    let :params do
+      {
+        :redis_port  => '6380',
+        :redis_saves => false
+      }
+    end
+
+    it do
+      should contain_file('redis_port_6380.conf').with_ensure('present')
+      should contain_file('redis_port_6380.conf').without_content(/^requirepass/)
+      should contain_file('redis_port_6380.conf').without_content(/^save 900 1$/)
+    end # it
+  end # context
+
+  context "On Debian systems with saves set to \['save 3600 1000000'\]" do
+    let :params do
+      {
+        :redis_port  => '7000',
+        :redis_saves => ['save 3600 1000000', 'save 17 42']
+      }
+    end # let
+
+    it do
+      should contain_file('redis_port_7000.conf').with_ensure('present')
+      should contain_file('redis_port_7000.conf').without_content(/^requirepass/)
+      should contain_file('redis_port_7000.conf').with_content(/^port 7000$/)
+      should contain_file('redis_port_7000.conf').with_content(/^save 3600 1000000$/)
+      should contain_file('redis_port_7000.conf').with_content(/^save 17 42$/)
+      should contain_file('redis_port_7000.conf').without_content(/^save 900 1$/)
+    end # it
+  end # context
 end # describe
